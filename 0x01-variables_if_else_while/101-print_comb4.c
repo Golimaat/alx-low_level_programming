9106f940086918d50dcad0579f7a2fe2d60af855
limaat berhe <berlimaat@gmail.com>
2022-09-12 16:03:32 -0700

    commited

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@
 * You can only use `putchar` to print to console .
 * you can only use `putchar` up to 6 times .
 * you are not allowed  to use any variables of type `char` .
-* Return :0
+* Return: 0
 */
 int main(void)
 {
