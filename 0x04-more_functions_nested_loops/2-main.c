38ceeb7ba1c4345b9870227cb2cb95a41b0a9e16
limaat berhe <berlimaat@gmail.com>
2022-09-15 11:41:13 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int mul(int a, int b);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: number1
+ * @b: number2
+ * Return: The multiplication result of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
