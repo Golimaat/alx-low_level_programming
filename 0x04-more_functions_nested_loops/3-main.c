eaf5d3bd766c32921c14fd849c0169df513a173f
limaat berhe <berlimaat@gmail.com>
2022-09-15 11:53:17 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	void print_numbers(void);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_number - prints integer from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
