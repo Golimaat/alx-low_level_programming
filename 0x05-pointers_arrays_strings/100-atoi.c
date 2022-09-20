65cfd0a31b98b16f3fed3a8669ea7ff68dee97bf
limaat berhe <berlimaat@gmail.com>
2022-09-20 02:44:30 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,29 +19,18 @@ int _isdigit(char s)
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int neg = 0,
-	int d;
-	int num = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[i])
-	{
-		if (s[i] == '-')
-		{
-			neg++;
-		}
-		else if (_isdigit(s[i]))
-		{
-			while (_isdigit(s[i]))
-			{
-				d = (s[i] - '0');
-				d = neg % 2 ? -d : d;
-				num = num * 10 + d;
-				i++;
-			}
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if
+			(*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
 			break;
-		}
-		i++;
-	}
-	return (num);
+	} while (*s++);
+
+	return (num * sign);
 }
