cf5c410f273cedbd1a0102eac51b41750ac2e269
limaat berhe <berlimaat@gmail.com>
2022-09-20 02:06:53 -0700

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
  * _puts - prints a string followed by a new line
- * @str: the string given as parameter 
+ * @str: the string given as parameter
  *
  * Return: 0
  */
 void _puts(char *str)
 {
-  int i;
-  
-  for (i = 0; str[i] != '\0'; i++)
-  {
-    _putchar(str[i]);
-  }
-  _putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
