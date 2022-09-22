be48216d8ea263c0debc6fac1dad6f7b6e4ae6f4
limaat berhe <berlimaat@gmail.com>
2022-09-22 11:53:34 -0700

    prints a[2]=98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+int n;
+int a[5];
+int *p;
 
-	a[2] = 1024;
+a[2] = 1024;
 
-	p = &n;
+p = &n;
 /*
  * Descrption - prints a[2] =98 followed by new line
  * Remember:
@@ -22,11 +22,11 @@ int main(void)
  * - you are not allowed to code anything else than this line of code
  * Return: 0
  */
-	*(p + 5) = 98;
+*(p + 5) = 98;
 
   /* ...so that this prints 98\n */
 
-	printf("a[2] = %d\n", a[2]);
+printf("a[2] = %d\n", a[2]);
 
-	return (0);
+return (0);
 }
